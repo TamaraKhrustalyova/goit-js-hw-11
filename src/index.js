acac54c702bbb82480a7fb8b56fd93ec714fcc87
TamaraKhrustalyova <tamara.khrustalyova@gmail.com>
2023-06-11 14:36:31 +0300

    update

--- src/index.js
+++ src/index.js
@@ -2,35 +2,48 @@ import Notiflix from 'notiflix';
 import axios from 'axios';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-import {fetchQuery} from "./js/api.js";
+
+import ImagesAPIService from "./js/api.js";
 
 const refs = {
-    formEl: document.querySelector('.search-form'),
-    galleryEl: document.querySelector('.gallery'),
-    inputEl: document.querySelector('input')
+    form: document.querySelector('.search-form'),
+    gallery: document.querySelector('.gallery'),
+    inputEl: document.querySelector('input'),
+    loadMoreBtn: document.querySelector('.load-more'),
 }
 
-refs.formEl.addEventListener('submit', onQuerySubmit)
+const imagesAPIService = new ImagesAPIService();
+
+refs.form.addEventListener('submit', onQuerySubmit);
+refs.loadMoreBtn.addEventListener('submit', onQuerySubmit);
 
 function onQuerySubmit(e){
     e.preventDefault()
     let searchedItem = refs.inputEl.value.trim();
-    console.log(searchedItem)
+    // console.log(searchedItem)
         if(searchedItem === "") {
+
+            updateQueryList('');
             Notiflix.Notify.warning('Please enter valid data to start the search.');
+            refs.loadMoreBtn.classList.add('is-hidden');
             return;
         }
         
-    fetchQuery(searchedItem).then(({hits}) => {
-        if(hits.length === 0) throw new Error(Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.'))
+        imagesAPIService.setSearchValue(searchedItem);
 
+        imagesAPIService.fetchQuery()
+        .then(({hits, totalHits}) => {
+
+        if(hits.length === 0) throw new Error(Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.'))
+            
+            Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`)
             return hits.reduce((markup, imageCard) =>
             markup + createItemMarkup(imageCard), '')
         })
         .then((markup) => updateQueryList(markup))
         .catch(onError)
-        .finally(() => refs.formEl.reset())
-}
+        .finally(() => refs.form.reset())
+}       
 
 const lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt'});
 lightbox.refresh();
@@ -50,10 +63,11 @@ function createItemMarkup({webformatURL, largeImageURL, tags, likes, views, comm
 }
 
 function updateQueryList(markup){
-    refs.galleryEl.innerHTML = markup
+    refs.gallery.innerHTML = markup
+    refs.loadMoreBtn.classList.remove('is-hidden')
 }
 
 function onError(error){
     updateQueryList(`${error.message}`)
+    refs.loadMoreBtn.classList.add('is-hidden')
 }
-
--- src/js/api.js
+++ src/js/api.js
@@ -1,11 +1,34 @@
 const API_KEY = '37157085-2fd193525cfc932d78d0f680d'
 const BASE_URL = `https://pixabay.com/api/`
 
-function fetchQuery(query){
-    const url = `${BASE_URL}?key=${API_KEY}&image_type=photo&orientation=horizontal&safesearch=true&q=${query}`
-    return fetch(url)
-    .then(res => res.json())
+export default class ImagesAPIService {
+    constructor() {
+        this.page = 1;
+        this.searchValue = "";
+    }
+
+    fetchQuery() {
+        const url = `${BASE_URL}?key=${API_KEY}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40&q=${this.searchValue}`
+        return fetch(url)
+        .then(res => res.json())
+        .then((data) => {
+            this.incrementPage();
+            return data;
+        });
+    }
+
+    setSearchValue(query) {
+        this.searchValue = query;
+    }
+
+    incrementPage() {
+        this.page += 1;
+    }
+
+    resetPage() {
+        this.page = 1;
+    }    
 }
 
-export {fetchQuery};
+
 
